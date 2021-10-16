c3b9d46d333b9e6d46ba4f2b2fd8d1682fd746a7
Aecio Prado <aecioprado@gmail.com>
2021-10-16 14:58:47 -0300

    add Matriz e MatrizTest

---
+++ src/test/java/com/school/cesar/MatrizTest.java
@@ -0,0 +1,59 @@
+package com.school.cesar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MatrizTest {
+
+	@Test
+	void testMatrizIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetColun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetElemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetElemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNegar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrizScanner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplicar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrizMatriz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testComparaIgual() {
+		fail("Not yet implemented"); 
+	}
+
+}
